15c78ccc012422daeab4dfca40ce534cf365618b
itgauy <torreslian@duck.com>
2024-07-16 18:10:37 +0800

    Game Room (Tentative)

--- frontend/src/components/GameRoom.jsx
+++ frontend/src/components/GameRoom.jsx
@@ -2,6 +2,18 @@ const GameRoom = () => {
   return (
     <>
       <div className="game-room-container">
+        <div className="pants-table">
+          <div className="category">P</div>
+          <div className="category">A</div>
+          <div className="category">N</div>
+          <div className="category">T</div>
+          <div className="category">S</div>
+        </div>
+
+        <div className="chat-with-timer">
+          <div className="chat">Chat messages go here</div>
+          <div className="timer">00:00</div>
+        </div>
 
       </div>
     </>
